ddd08cf0b2f9653f5f32c4a94bbb4755f3cccd21
Charlie Campbell <chacha9669@hotmail.com>
2024-08-05 14:40:56 +1000

    Spear minigame progress

---
+++ Assets/Scripts/Spearing/SpearMinigame.cs
@@ -0,0 +1,37 @@
+using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpearMinigame : MonoBehaviour
+{
+    [SerializeField] private GameObject cursor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
+        cursor.transform.position = Input.mousePosition;
+        Vector2 vectorFromOrigin = new Vector2(cursor.transform.localPosition.x, cursor.transform.localPosition.y);
+        if (vectorFromOrigin.magnitude < 40)
+        {
+            Mouse.current.WarpCursorPosition(new Vector2(Random.Range(0, Screen.width), Random.Range(0, Screen.height)));
+        }
+
+
+        
+    }
+
+}
