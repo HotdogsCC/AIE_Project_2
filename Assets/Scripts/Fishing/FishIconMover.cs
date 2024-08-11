afbc8b2367ab77c3646e95786793a6dcabeeac4b
Charlie Campbell <chacha9669@hotmail.com>
2024-08-11 14:48:00 +1000

    Fish icon moves randomly
    i also made the water more transparent

---
+++ Assets/Scripts/Fishing/FishIconMover.cs
@@ -0,0 +1,52 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FishIconMover : MonoBehaviour
+{
+    [Header("Object References")]
+    [SerializeField] private Slider slider;
+
+    [Header("Adjustable Numbers")]
+    [SerializeField] private float moveSpeed = 1f;
+    [Range(0, 100)]
+    [SerializeField] private int chanceOfChangingDirection = 10;
+
+    [Header("Non Adjustable Number")]
+    [SerializeField] private bool movingUp = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (movingUp)
+        {
+            slider.value += moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            slider.value -= moveSpeed * Time.deltaTime;
+        }
+
+        if(slider.value <= 0)
+        {
+            slider.value = 1;
+            movingUp = true;
+        }
+        if (slider.value >= 100)
+        {
+            slider.value = 99;
+            movingUp = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (chanceOfChangingDirection > Random.Range(0, 100))
+        {
+            movingUp = !movingUp;
+        }
+    }
+}
