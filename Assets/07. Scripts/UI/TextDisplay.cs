6091487503c0034d8d414281b92ff3f186681e37
Charlie Campbell <chacha9669@hotmail.com>
2024-09-02 12:26:58 +1000

    added player model to prefab

--- Assets/07. Scripts/UI/TextDisplay.cs
+++ Assets/07. Scripts/UI/TextDisplay.cs
@@ -15,21 +15,17 @@ public class TextDisplay : MonoBehaviour
     public void DisplayText(string _text)
     {
         text.text = _text;
-        Debug.Log("1 ran");
     }
 
     public void DisplayText(string _text, float time)
     {
-        Debug.Log("2");
         DisplayText(_text);
         StartCoroutine(WaitForAMo(time));
     }
 
     private IEnumerator WaitForAMo(float time)
     {
-        Debug.Log("3");
         yield return new WaitForSeconds(time);
-        Debug.Log("4");
         DisplayText("");
     }
 }
