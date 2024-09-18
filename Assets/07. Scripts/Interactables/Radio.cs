573ad24051310da72782719ae8acbb5b1d972a48
Charlie Campbell <chacha9669@hotmail.com>
2024-09-18 22:21:53 +1000

    Radio opening and closing

--- Assets/07. Scripts/Interactables/Radio.cs
+++ Assets/07. Scripts/Interactables/Radio.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class Radio : Interactable
 {
+    [SerializeField] private GameObject radioUI;
+
     public override void Interact()
     {
         Debug.Log("opened radio");
+        radioUI.SetActive(true);
+        Pausing.FreezePlayer();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("closed radio");
+            radioUI.SetActive(false);
+            Pausing.ResumePlayer();
+        }
     }
 }
