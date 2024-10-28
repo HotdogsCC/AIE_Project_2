d3dfd8d8edc01311ef6d80584a7e6cba21628609
Charlie Campbell <chacha9669@hotmail.com>
2024-10-29 09:31:22 +1100

    Added new fish types to upgrade scriptable object

--- Assets/07. Scripts/Interactables/Radio/DialogueRadio.cs
+++ Assets/07. Scripts/Interactables/Radio/DialogueRadio.cs
@@ -92,72 +92,143 @@ public class DialogueRadio : MonoBehaviour
         upgradeButtons.SetActive(true);
         mainMenuButtons.SetActive(false);
 
-        //sets text to upgrade cost
-        text.text = @$"Speed: {swimUpgrades[speedLevel].GetSalmonCost()} salmon, {swimUpgrades[speedLevel].GetBabaroosaCost()} babaroosa
-Breath: {breatheUpgrades[breatheLevel].GetSalmonCost()} salmon, {breatheUpgrades[breatheLevel].GetBabaroosaCost()} babaroosa
-Health: {healthUpgrades[healthLevel].GetSalmonCost()} salmon, {healthUpgrades[healthLevel].GetBabaroosaCost()} babaroosa
-
-You have: {Stats.GetFishInv(0)} salmon, {Stats.GetFishInv(1)} babaroosa";
-
         //doesn't display the cost if it is 0
         text.text = "Speed: ";
-        if (swimUpgrades[speedLevel].GetSalmonCost() != 0)
+        if (swimUpgrades[speedLevel].GetCost(0) != 0)
         {
-            text.text += $"{swimUpgrades[speedLevel].GetSalmonCost()} salmon, ";
+            text.text += $"{swimUpgrades[speedLevel].GetCost(0)} salmon, ";
         }
-        if (swimUpgrades[speedLevel].GetBabaroosaCost() != 0)
+        if (swimUpgrades[speedLevel].GetCost(1) != 0)
         {
-            text.text += $"{swimUpgrades[speedLevel].GetBabaroosaCost()} babaroosa, ";
+            text.text += $"{swimUpgrades[speedLevel].GetCost(1)} babaroosa, ";
+        }
+        if (swimUpgrades[speedLevel].GetCost(2) != 0)
+        {
+            text.text += $"{swimUpgrades[speedLevel].GetCost(2)} face fish, ";
+        }
+        if (swimUpgrades[speedLevel].GetCost(3) != 0)
+        {
+            text.text += $"{swimUpgrades[speedLevel].GetCost(3)} coral fish, ";
+        }
+        if (swimUpgrades[speedLevel].GetCost(4) != 0)
+        {
+            text.text += $"{swimUpgrades[speedLevel].GetCost(4)} box fish, ";
+        }
+        if (swimUpgrades[speedLevel].GetCost(5) != 0)
+        {
+            text.text += $"{swimUpgrades[speedLevel].GetCost(2)} lampery, ";
+        }
+        if (swimUpgrades[speedLevel].GetCost(6) != 0)
+        {
+            text.text += $"{swimUpgrades[speedLevel].GetCost(6)} eyeless fish, ";
+        }
+        if (swimUpgrades[speedLevel].GetCost(7) != 0)
+        {
+            text.text += $"{swimUpgrades[speedLevel].GetCost(7)} lure jaw, ";
         }
 
         text.text += "\nBreath: ";
-        if (breatheUpgrades[breatheLevel].GetSalmonCost() != 0)
+        if (breatheUpgrades[breatheLevel].GetCost(0) != 0)
         {
-            text.text += $"{breatheUpgrades[breatheLevel].GetSalmonCost()} salmon, ";
+            text.text += $"{breatheUpgrades[breatheLevel].GetCost(0)} salmon, ";
         }
-        if (breatheUpgrades[breatheLevel].GetBabaroosaCost() != 0)
+        if (breatheUpgrades[breatheLevel].GetCost(1) != 0)
         {
-            text.text += $"{breatheUpgrades[breatheLevel].GetBabaroosaCost()} babaroosa, ";
+            text.text += $"{breatheUpgrades[breatheLevel].GetCost(1)} babaroosa, ";
+        }
+        if (breatheUpgrades[breatheLevel].GetCost(2) != 0)
+        {
+            text.text += $"{breatheUpgrades[breatheLevel].GetCost(2)} face fish, ";
+        }
+        if (breatheUpgrades[breatheLevel].GetCost(3) != 0)
+        {
+            text.text += $"{breatheUpgrades[breatheLevel].GetCost(3)} coral fish, ";
+        }
+        if (breatheUpgrades[breatheLevel].GetCost(4) != 0)
+        {
+            text.text += $"{breatheUpgrades[breatheLevel].GetCost(4)} box fish, ";
+        }
+        if (breatheUpgrades[breatheLevel].GetCost(5) != 0)
+        {
+            text.text += $"{breatheUpgrades[breatheLevel].GetCost(2)} lampery, ";
+        }
+        if (breatheUpgrades[breatheLevel].GetCost(6) != 0)
+        {
+            text.text += $"{breatheUpgrades[breatheLevel].GetCost(6)} eyeless fish, ";
+        }
+        if (breatheUpgrades[breatheLevel].GetCost(7) != 0)
+        {
+            text.text += $"{breatheUpgrades[breatheLevel].GetCost(7)} lure jaw, ";
         }
 
         text.text += "\nHealth: ";
-        if (healthUpgrades[healthLevel].GetSalmonCost() != 0)
+        if (healthUpgrades[healthLevel].GetCost(0) != 0)
         {
-            text.text += $"{healthUpgrades[healthLevel].GetSalmonCost()} salmon, ";
+            text.text += $"{healthUpgrades[healthLevel].GetCost(0)} salmon, ";
         }
-        if (healthUpgrades[healthLevel].GetBabaroosaCost() != 0)
+        if (healthUpgrades[healthLevel].GetCost(1) != 0)
         {
-            text.text += $"{healthUpgrades[healthLevel].GetBabaroosaCost()} babaroosa, ";
+            text.text += $"{healthUpgrades[healthLevel].GetCost(1)} babaroosa, ";
+        }
+        if (healthUpgrades[healthLevel].GetCost(2) != 0)
+        {
+            text.text += $"{healthUpgrades[healthLevel].GetCost(2)} face fish, ";
+        }
+        if (healthUpgrades[healthLevel].GetCost(3) != 0)
+        {
+            text.text += $"{healthUpgrades[healthLevel].GetCost(3)} coral fish, ";
+        }
+        if (healthUpgrades[healthLevel].GetCost(4) != 0)
+        {
+            text.text += $"{healthUpgrades[healthLevel].GetCost(4)} box fish, ";
+        }
+        if (healthUpgrades[healthLevel].GetCost(5) != 0)
+        {
+            text.text += $"{healthUpgrades[healthLevel].GetCost(2)} lampery, ";
+        }
+        if (healthUpgrades[healthLevel].GetCost(6) != 0)
+        {
+            text.text += $"{healthUpgrades[healthLevel].GetCost(6)} eyeless fish, ";
+        }
+        if (healthUpgrades[healthLevel].GetCost(7) != 0)
+        {
+            text.text += $"{healthUpgrades[healthLevel].GetCost(7)} lure jaw, ";
         }
 
-        text.text += $"\n\nYou have: {Stats.GetFishInv(0)} salmon, {Stats.GetFishInv(1)} babaroosa";
+        text.text += $"\n\nYou have: {Stats.GetFishInv(0)} salmon, {Stats.GetFishInv(1)} babaroosa, {Stats.GetFishInv(2)} face fish, {Stats.GetFishInv(3)} coral fish, {Stats.GetFishInv(4)} box fish, {Stats.GetFishInv(5)} lampery, {Stats.GetFishInv(6)} eyeless fish, {Stats.GetFishInv(7)} lure jaw";
 
         //sets interactivity of button based on money
-        if (Stats.GetFishInv(0) >= swimUpgrades[speedLevel].GetSalmonCost() && Stats.GetFishInv(1) >= swimUpgrades[speedLevel].GetBabaroosaCost())
-        {
-            speedUpgradeButton.interactable = true;
-        }
-        else
-        {
-            speedUpgradeButton.interactable = false;
-        }
-
-        if (Stats.GetFishInv(0) >= breatheUpgrades[breatheLevel].GetSalmonCost() && Stats.GetFishInv(1) >= breatheUpgrades[breatheLevel].GetBabaroosaCost())
-        {
-            breatheUpgradeButton.interactable = true;
-        }
-        else
-        {
-            breatheUpgradeButton.interactable = false;
+        speedUpgradeButton.interactable = true;
+        for (int i = 0; i < 8; i++)
+        { 
+            //if you dont have enough money, break
+            if(Stats.GetFishInv(i) < swimUpgrades[speedLevel].GetCost(i))
+            {
+                speedUpgradeButton.interactable = false;
+                break;
+            }
         }
 
-        if (Stats.GetFishInv(0) >= healthUpgrades[healthLevel].GetSalmonCost() && Stats.GetFishInv(1) >= healthUpgrades[healthLevel].GetBabaroosaCost())
+        breatheUpgradeButton.interactable = true;
+        for (int i = 0; i < 8; i++)
         {
-            healthUpgradeButton.interactable = true;
+            //if you dont have enough money, break
+            if (Stats.GetFishInv(i) < breatheUpgrades[breatheLevel].GetCost(i))
+            {
+                breatheUpgradeButton.interactable = false;
+                break;
+            }
         }
-        else
+
+        healthUpgradeButton.interactable = true;
+        for (int i = 0; i < 8; i++)
         {
-            healthUpgradeButton.interactable = false;
+            //if you dont have enough money, break
+            if (Stats.GetFishInv(i) < healthUpgrades[healthLevel].GetCost(i))
+            {
+                healthUpgradeButton.interactable = false;
+                break;
+            }
         }
 
     }
@@ -165,8 +236,14 @@ You have: {Stats.GetFishInv(0)} salmon, {Stats.GetFishInv(1)} babaroosa";
     public void SpeedUpgrade()
     {
         FindObjectOfType<GroundWaterManager>().waterMoveSpeed = swimUpgrades[speedLevel].GetResultOfUpgrade();
-        Stats.RemoveFishInv(0, swimUpgrades[speedLevel].GetSalmonCost());
-        Stats.RemoveFishInv(1, swimUpgrades[speedLevel].GetBabaroosaCost());
+        Stats.RemoveFishInv(0, swimUpgrades[speedLevel].GetCost(0));
+        Stats.RemoveFishInv(1, swimUpgrades[speedLevel].GetCost(1));
+        Stats.RemoveFishInv(1, swimUpgrades[speedLevel].GetCost(2));
+        Stats.RemoveFishInv(1, swimUpgrades[speedLevel].GetCost(3));
+        Stats.RemoveFishInv(1, swimUpgrades[speedLevel].GetCost(4));
+        Stats.RemoveFishInv(1, swimUpgrades[speedLevel].GetCost(5));
+        Stats.RemoveFishInv(1, swimUpgrades[speedLevel].GetCost(6));
+        Stats.RemoveFishInv(1, swimUpgrades[speedLevel].GetCost(7));
         speedLevel++;
         MainMenu();
     }
@@ -174,8 +251,14 @@ You have: {Stats.GetFishInv(0)} salmon, {Stats.GetFishInv(1)} babaroosa";
     public void BreatheUpgrade()
     {
         FindObjectOfType<Breathe>().SetBreatheTime(breatheUpgrades[breatheLevel].GetResultOfUpgrade());
-        Stats.RemoveFishInv(0, breatheUpgrades[breatheLevel].GetSalmonCost());
-        Stats.RemoveFishInv(1, breatheUpgrades[breatheLevel].GetBabaroosaCost());
+        Stats.RemoveFishInv(0, breatheUpgrades[breatheLevel].GetCost(0));
+        Stats.RemoveFishInv(1, breatheUpgrades[breatheLevel].GetCost(1));
+        Stats.RemoveFishInv(1, breatheUpgrades[breatheLevel].GetCost(2));
+        Stats.RemoveFishInv(1, breatheUpgrades[breatheLevel].GetCost(3));
+        Stats.RemoveFishInv(1, breatheUpgrades[breatheLevel].GetCost(4));
+        Stats.RemoveFishInv(1, breatheUpgrades[breatheLevel].GetCost(5));
+        Stats.RemoveFishInv(1, breatheUpgrades[breatheLevel].GetCost(6));
+        Stats.RemoveFishInv(1, breatheUpgrades[breatheLevel].GetCost(7));
         breatheLevel++;
         MainMenu();
     }
@@ -183,8 +266,14 @@ You have: {Stats.GetFishInv(0)} salmon, {Stats.GetFishInv(1)} babaroosa";
     public void HealthUpgrade()
     {
         FindObjectOfType<Health>().SetMaxHealth(healthUpgrades[healthLevel].GetResultOfUpgrade());
-        Stats.RemoveFishInv(0, healthUpgrades[healthLevel].GetSalmonCost());
-        Stats.RemoveFishInv(1, healthUpgrades[healthLevel].GetBabaroosaCost());
+        Stats.RemoveFishInv(0, healthUpgrades[healthLevel].GetCost(0));
+        Stats.RemoveFishInv(1, healthUpgrades[healthLevel].GetCost(1));
+        Stats.RemoveFishInv(1, healthUpgrades[healthLevel].GetCost(2));
+        Stats.RemoveFishInv(1, healthUpgrades[healthLevel].GetCost(3));
+        Stats.RemoveFishInv(1, healthUpgrades[healthLevel].GetCost(4));
+        Stats.RemoveFishInv(1, healthUpgrades[healthLevel].GetCost(5));
+        Stats.RemoveFishInv(1, healthUpgrades[healthLevel].GetCost(6));
+        Stats.RemoveFishInv(1, healthUpgrades[healthLevel].GetCost(7));
         healthLevel++;
         MainMenu();
     }
--- Assets/16. Upgrades/UpgradeRequirements.cs
+++ Assets/16. Upgrades/UpgradeRequirements.cs
@@ -7,18 +7,41 @@ public class UpgradeRequirements : ScriptableObject
     [Header("Cost of Upgrade")]
     [SerializeField] private byte salmon;
     [SerializeField] private byte babaroosa;
+    [SerializeField] private byte faceFish;
+    [SerializeField] private byte coralFish;
+    [SerializeField] private byte boxFish;
+    [SerializeField] private byte lampery;
+    [SerializeField] private byte eyeless;
+    [SerializeField] private byte lureJaw;
 
     [Header("Result of Upgrade")]
     [SerializeField] private float value;
 
-    public byte GetSalmonCost()
-    {
-        return salmon;
-    }
 
-    public byte GetBabaroosaCost()
+
+    public byte GetCost(int index)
     {
-        return babaroosa;
+        switch (index)
+        {
+            case 0:
+                return salmon;
+            case 1:
+                return babaroosa;
+            case 2:
+                return faceFish;
+            case 3:
+                return coralFish;
+            case 4:
+                return boxFish;
+            case 5:
+                return lampery;
+            case 6:
+                return eyeless;
+            case 7:
+                return lureJaw;
+            default:
+                return 0;
+        }
     }
 
     public float GetResultOfUpgrade()
